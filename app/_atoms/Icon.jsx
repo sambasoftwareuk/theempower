86618b48184a9249d3e7083adc79314451cae89c
herrbaha <baha_i@hotmail.com>
2025-04-16 22:29:14 +0200

    create button Atoms and colors

--- app/_atoms/Icon.jsx
+++ app/_atoms/Icon.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Icon = ({ variant: Variant, size = 24, color = 'currentColor' }) => {
-    return (
-      <Variant 
-        style={{ width: size, height: size, fill: color }}
-      />
-    );
-  };
+const Icon = ({ variant: Variant, size = 24, color = 'text-current', className = '' }) => {
+  return (
+    <Variant 
+      style={{ width: size, height: size }}
+      className={`${color} ${className}`}
+    />
+  );
+};
   
 
 export default Icon;
--- app/_atoms/Icons.jsx
+++ app/_atoms/Icons.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Search = ({ style }) => {
+export const Search = ({ style, className='' }) => {
   return (
     <svg 
       xmlns="http://www.w3.org/2000/svg" 
@@ -9,36 +9,39 @@ export const Search = ({ style }) => {
       width="24px" 
       fill="currentColor"
       style={style}
+      className={className}
     >
       <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
     </svg>
   );
 };
 
-export const Cart = ({ style }) => {
+export const Cart = ({ style, className=''}) => {
   return (
     <svg 
       xmlns="http://www.w3.org/2000/svg" 
       height="24px" 
       viewBox="0 -960 960 960" 
       width="24px" 
-      fill="#1f1f1f" 
+      fill="currentColor"
       style={style}
+      className={className}
     >
       <path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
     </svg>
   );
 };
 
-export const Globe = ({ style, fill = 'currentColor' }) => {
+export const Globe = ({ style, className='' }) => {
   return (
     <svg 
       xmlns="http://www.w3.org/2000/svg" 
       viewBox="0 -960 960 960" 
       width="24px" 
       height="24px" 
-      fill={fill}
+      fill="currentColor"
       style={style}
+      className={className}
     >
       <path d="M480-80q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-155.5t86-127Q252-817 325-848.5T480-880q83 0 155.5 31.5t127 86q54.5 54.5 86 127T880-480q0 82-31.5 155t-86 127.5q-54.5 54.5-127 86T480-80Zm0-82q26-36 45-75t31-83H404q12 44 31 83t45 75Zm-104-16q-18-33-31.5-68.5T322-320H204q29 50 72.5 87t99.5 55Zm208 0q56-18 99.5-55t72.5-87H638q-9 38-22.5 73.5T584-178ZM170-400h136q-3-20-4.5-39.5T300-480q0-21 1.5-40.5T306-560H170q-5 20-7.5 39.5T160-480q0 21 2.5 40.5T170-400Zm216 0h188q3-20 4.5-39.5T580-480q0-21-1.5-40.5T574-560H386q-3 20-4.5 39.5T380-480q0 21 1.5 40.5T386-400Zm268 0h136q5-20 7.5-39.5T800-480q0-21-2.5-40.5T790-560H654q3 20 4.5 39.5T660-480q0 21-1.5 40.5T654-400Zm-16-240h118q-29-50-72.5-87T584-782q18 33 31.5 68.5T638-640Zm-234 0h152q-12-44-31-83t-45-75q-26 36-45 75t-31 83Zm-200 0h118q9-38 22.5-73.5T376-782q-56 18-99.5 55T204-640Z"/>
     </svg>
@@ -46,30 +49,32 @@ export const Globe = ({ style, fill = 'currentColor' }) => {
 };
 
 
-export const Star = ({ style, fill = 'currentColor' }) => {
+export const Star = ({ style, className='' }) => {
   return (
     <svg 
       xmlns="http://www.w3.org/2000/svg" 
       viewBox="0 -960 960 960" 
       width="24px" 
       height="24px" 
-      fill={fill} 
+      fill="currentColor" 
       style={style}
+      className={className}
     >
       <path d="m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143ZM233-120l65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Zm247-350Z" />
     </svg>
   );
 };
 
-export const HalfStar= ({ style, fill = 'currentColor' }) => {
+export const HalfStar= ({ style, className='' }) => {
   return (
     <svg 
       xmlns="http://www.w3.org/2000/svg" 
       viewBox="0 -960 960 960" 
       width="24px" 
       height="24px" 
-      fill={fill} 
+      fill="currentColor" 
       style={style}
+      className={className}
     >
       <path d="m606-286-33-144 111-96-146-13-58-136v312l126 77ZM233-120l65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"/>
     </svg>
