5cddcfe31f9f3088c32d572c2527aca9467e550a
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2025-04-16 14:16:06 +0100

    input atom completed

--- app/page.js
+++ app/page.js
@@ -1,26 +1,15 @@
+"use client";
 import Icon from "./_atoms/Icon";
 import { Cart, Search, Globe, Star, HalfStar } from "./_atoms/Icons";
-import Input from "../app/_atoms/input";
 
 export default function Home() {
   return (
-    <div>
+    <div className="p-4">
       <Icon variant={Cart} size={32} color="#000000" />
       <Icon variant={Search} size={32} color="red" />
       <Icon variant={Globe} size={32} color="grey" />
       <Icon variant={Star} size={32} color="green" />
       <Icon variant={HalfStar} size={32} color="blue" />
-
-      <div>
-        Empower Project
-        <Input placeholder="Enter .." variant="basic" />
-        {/* `     <Input
-        placeholder="Enter ..."
-        variant='iconEnd'
-        icon={search}
-        onIconClick={()=>console.log('clear click')}
-      />` */}
-      </div>
     </div>
   );
 }
