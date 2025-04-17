81da3cf0f25076e1b797153aa7f4fdf16a91f699
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2025-04-17 10:32:31 +0100

    atom input component updated

--- app/_atoms/inputs.jsx
+++ app/_atoms/inputs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Icon from "./Icon";
 export const InputBasic = ({ className = "", ...props }) => (
   <input
-    className={` w-full border rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-purple-400 ${className}`}
+    className={`w-full border rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-purple-400 ${className}`}
     {...props}
   />
 );
@@ -15,7 +15,7 @@ export const InputBasicWithIcon = ({
     className={`flex items-center w-full border rounded-md px-4 py-2 text-base focus-within:ring-2 focus-within:ring-purple-400 ${className}`}
   >
     <input className="flex-1 border-none bg-transparent focus:outline-none text-base placeholder:text-gray-400" />
-    {Icon && <Icon size={20} classNamemi-2 text-gray-500 />}
+    {Icon && <Icon size={20} />}
   </div>
 );
 
@@ -26,7 +26,7 @@ export const InputWithClickableIcon = ({
   ...props
 }) => (
   <div
-    className={`flex items-center border rounded-full pr-1  py-0.5 focus-within:ring-2 focus-within:ring-purple-400 ${className}`}
+    className={`flex items-center border rounded-full pr-1 py-0.5 focus-within:ring-2 focus-within:ring-purple-400 ${className}`}
   >
     <input
       className="flex-1 border-none px-4 py-2 text-sm focus:outline-none"
@@ -36,7 +36,7 @@ export const InputWithClickableIcon = ({
       <button
         type="button"
         onClick={onIconClick}
-        className="p-1.5  cursor-pointer bg-purple-100 rounded-full transition"
+        className="p-1.5 cursor-pointer bg-purple-100 rounded-full transition"
       >
         <Icon variant={icon} color="#fff" />
       </button>
