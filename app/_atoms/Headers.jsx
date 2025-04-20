7352c544ae22188f8f5f82069770405a25babd0d
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2025-04-20 21:05:15 +0100

    header adjusted and colors applied

--- app/_atoms/Headers.jsx
+++ app/_atoms/Headers.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
-export const HeaderH1 = ({children}) => {
+export const Header1 = ({children}) => {
   return (
     <h1 className="text-3xl font-bold text-primary">{children}</h1>
   )
 }
 
-export const HeaderH2 = ({children}) => {
+export const Header2 = ({children}) => {
     return (
       <h2 className="text-2xl font-bold text-primary">{children}</h2>
     )
 }
 
-export const HeaderH3 = ({children}) => {
+export const Header3 = ({children}) => {
     return (
-      <h2 className="text-xl font-bold text-primary">{children}</h2>
+      <h3 className="text-xl font-bold text-primary">{children}</h3>
     )
 }
