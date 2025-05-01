a3b3b07365045b4ded3bb3e72f00b9ae5455a7ec
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2025-05-01 09:32:18 +0100

    custom color and custom size applied

--- app/_atoms/Headers.jsx
+++ app/_atoms/Headers.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
-export const Header1 = ({children}) => {
+export const Header1 = ({children, className = ""}) => {
   return (
-    <h1 className="text-3xl font-bold text-primary">{children}</h1>
+    <h1 className={`text-3xl font-bold text-primary ${className}`}>{children}</h1>
   )
 }
 
-export const Header2 = ({children}) => {
+export const Header2 = ({children, className = ""}) => {
     return (
-      <h2 className="text-2xl font-bold text-primary">{children}</h2>
+      <h2 className={`text-2xl font-bold text-primary ${className}`}>{children}</h2>
     )
 }
 
-export const Header3 = ({children}) => {
+export const Header3 = ({children, className= ""}) => {
     return (
-      <h3 className="text-xl font-bold text-primary">{children}</h3>
+      <h3 className={`text-xl font-bold text-primary ${className}`}>{children}</h3>
     )
 }
