61d1992ebbc3ad65f5f5b86eaf65317a695f14ae
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2025-05-12 18:50:43 +0100

    cardWithIcon module complete

--- app/page.js
+++ app/page.js
@@ -1,3 +1,21 @@
+import CourseCard from "./_molecules/CourseCard";
+import { CardWithIcon } from "./_molecules/cardWithIcon";
+import { Certificate } from "./_atoms/Icons";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="m-4 ">
+      <CourseCard />
+      <div className="mt-5">
+        <CardWithIcon
+          icon={Certificate}
+          title="Hands-on training"
+          description="Upskill effectively with AI-powered coding exercises, practice tests, and quizzes."
+          badge="Enterprise Plan"
+          linkText="Explore courses"
+          linkHref="#"
+        />
+      </div>
+    </div>
+  );
 }
