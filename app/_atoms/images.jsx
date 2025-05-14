a0d35c659b83f8674f9bec5eee5a8e5b478fd77c
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2025-05-14 14:41:46 +0100

    image functions repeatation removed

--- app/_atoms/images.jsx
+++ app/_atoms/images.jsx
@@ -1,20 +1,20 @@
 import Image from 'next/image'
 import React from 'react'
 
+const src = (imageLink) => imageLink && imageLink.trim() !== "" ? imageLink : "/generic-image.png";
+const imageSlug = (imageLink) => imageLink ? imageLink.split('/').pop().replace(/\.[^/.]+$/, "") : "generic-image"
+
 export const SliderImage = ({ imageLink }) => {
-  const src = imageLink && imageLink.trim() !== "" ? imageLink : "/generic-image.png";
-  const imageSlug = imageLink ? imageLink.split('/').pop().replace(/\.[^/.]+$/, "") : "generic-image"
-  console.log(src);
   
   return (
     <div className="mx-auto w-full max-w-[1350px]">
       <div className="custom:block relative h-[250px] w-full overflow-hidden">
         <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[1350px] h-full">
           <Image
-            src={src}
-            alt={`slider-image-${imageSlug}`}
+            src={src(imageLink)}
+            alt={`slider-image-${imageSlug(imageLink)}`}
             fill
-            className={src == "/generic-image.png" ? "object-contain bg-gray-300" : "object-cover"}
+            className={src(imageLink) == "/generic-image.png" ? "object-contain bg-gray-300" : "object-cover"}
           />
         </div>
       </div>
@@ -23,18 +23,16 @@ export const SliderImage = ({ imageLink }) => {
 }
 
 export const CardImage = ({ imageLink }) => {
-  const src = imageLink && imageLink.trim() !== "" ? imageLink : "/generic-image.png";
-  const imageSlug = imageLink ? imageLink.split('/').pop().replace(/\.[^/.]+$/, "") : "generic-image"
-  console.log(src);
+  
     return (
       <div className="mx-auto w-full max-w-[240px]">
         <div className="custom:block relative w-[240px] h-[135px] overflow-hidden rounded-t-md">
           <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[240px] h-full">
             <Image
-              src={src}
-              alt={`card-image-${imageSlug}`}
+              src={src(imageLink)}
+              alt={`card-image-${imageSlug(imageLink)}`}
               fill
-              className={src == "/generic-image.png" ? "object-contain bg-gray-300" : "object-cover"}
+              className={src(imageLink) == "/generic-image.png" ? "object-contain bg-gray-300" : "object-cover"}
             />
           </div>
         </div>
@@ -44,17 +42,14 @@ export const CardImage = ({ imageLink }) => {
 
 
 export const LogoImage = ({ imageLink = "" }) => {
-  const src = imageLink && imageLink.trim() !== "" ? imageLink : "/generic-image.png";
-  const imageSlug = imageLink ? imageLink.split('/').pop().replace(/\.[^/.]+$/, "") : "generic-image"
-  console.log(src);
-
+  
   return (
     <div className='relative w-[200px] h-[80px] overflow-hidden '>
       <Image
-        src={src}
-        alt={`logo-image-${imageSlug}`}
+        src={src(imageLink)}
+        alt={`logo-image-${imageSlug(imageLink)}`}
         fill
-        className={`object-contain ${src == "/generic-image.png" ? "bg-gray-300" : ""}`}
+        className={`object-contain ${src(imageLink) == "/generic-image.png" ? "bg-gray-300" : ""}`}
       />
     </div>
   );
@@ -62,15 +57,12 @@ export const LogoImage = ({ imageLink = "" }) => {
 
 
 export const ProfileImage = ({ imageLink = "" }) => {
-  const src = imageLink && imageLink.trim() !== "" ? imageLink : "/question-mark.png";
-  const imageSlug = imageLink ? imageLink.split('/').pop().replace(/\.[^/.]+$/, "") : "generic-image"
-  console.log(src);
-
+  
   return (
     <div className='relative w-[80px] h-[80px] overflow-hidden'>
       <Image
-        src={src}
-        alt={`profile-image-${imageSlug}`}
+        src={src(imageLink)}
+        alt={`profile-image-${imageSlug(imageLink)}`}
         fill
         className="object-contain border-2 border-black rounded-full bg-gray-200"
       />
