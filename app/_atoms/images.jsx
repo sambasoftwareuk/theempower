e0a69440d1b2e5c05b52622d60553f9a8a7f935c
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2025-05-15 09:26:56 +0100

    useMemo applied

--- app/_atoms/images.jsx
+++ app/_atoms/images.jsx
@@ -1,20 +1,21 @@
 import Image from 'next/image'
-import React from 'react'
+import React, { useMemo } from 'react'
 
-const src = (imageLink) => imageLink && imageLink.trim() !== "" ? imageLink : "/generic-image.png";
-const imageSlug = (imageLink) => imageLink ? imageLink.split('/').pop().replace(/\.[^/.]+$/, "") : "generic-image"
+const getSrc = (imageLink) => imageLink && imageLink.trim() !== "" ? imageLink : "/generic-image.png";
+const getImageSlug = (imageLink) => imageLink ? imageLink.split('/').pop().replace(/\.[^/.]+$/, "") : "generic-image"
 
 export const SliderImage = ({ imageLink }) => {
-  
+  const src = useMemo(() => getSrc(imageLink), [imageLink]);
+  const imageSlug = useMemo(() => getImageSlug(imageLink), [imageLink]);
   return (
     <div className="mx-auto w-full max-w-[1350px]">
       <div className="custom:block relative h-[250px] w-full overflow-hidden">
         <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[1350px] h-full">
           <Image
-            src={src(imageLink)}
-            alt={`slider-image-${imageSlug(imageLink)}`}
+            src={src}
+            alt={`slider-image-${imageSlug}`}
             fill
-            className={src(imageLink) == "/generic-image.png" ? "object-contain bg-gray-300" : "object-cover"}
+            className={src == "/generic-image.png" ? "object-contain bg-gray-300" : "object-cover"}
           />
         </div>
       </div>
@@ -23,16 +24,17 @@ export const SliderImage = ({ imageLink }) => {
 }
 
 export const CardImage = ({ imageLink }) => {
-  
+  const src = useMemo(() => getSrc(imageLink), [imageLink]);
+  const imageSlug = useMemo(() => getImageSlug(imageLink), [imageLink]);
     return (
       <div className="mx-auto w-full max-w-[240px]">
         <div className="custom:block relative w-[240px] h-[135px] overflow-hidden rounded-t-md">
           <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[240px] h-full">
             <Image
-              src={src(imageLink)}
-              alt={`card-image-${imageSlug(imageLink)}`}
+              src={src}
+              alt={`card-image-${imageSlug}`}
               fill
-              className={src(imageLink) == "/generic-image.png" ? "object-contain bg-gray-300" : "object-cover"}
+              className={src == "/generic-image.png" ? "object-contain bg-gray-300" : "object-cover"}
             />
           </div>
         </div>
@@ -42,14 +44,15 @@ export const CardImage = ({ imageLink }) => {
 
 
 export const LogoImage = ({ imageLink = "" }) => {
-  
+  const src = useMemo(() => getSrc(imageLink), [imageLink]);
+  const imageSlug = useMemo(() => getImageSlug(imageLink), [imageLink]);
   return (
     <div className='relative w-[200px] h-[80px] overflow-hidden '>
       <Image
-        src={src(imageLink)}
-        alt={`logo-image-${imageSlug(imageLink)}`}
+        src={src}
+        alt={`logo-image-${imageSlug}`}
         fill
-        className={`object-contain ${src(imageLink) == "/generic-image.png" ? "bg-gray-300" : ""}`}
+        className={`object-contain ${src == "/generic-image.png" ? "bg-gray-300" : ""}`}
       />
     </div>
   );
@@ -57,12 +60,13 @@ export const LogoImage = ({ imageLink = "" }) => {
 
 
 export const ProfileImage = ({ imageLink = "" }) => {
-  
+  const src = useMemo(() => getSrc(imageLink), [imageLink]);
+  const imageSlug = useMemo(() => getImageSlug(imageLink), [imageLink]);
   return (
     <div className='relative w-[80px] h-[80px] overflow-hidden'>
       <Image
-        src={src(imageLink)}
-        alt={`profile-image-${imageSlug(imageLink)}`}
+        src={src}
+        alt={`profile-image-${imageSlug}`}
         fill
         className="object-contain border-2 border-black rounded-full bg-gray-200"
       />
