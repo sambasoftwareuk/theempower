cba4aeb5c08db4449bfd2e7fb1d60ef171e650a2
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2025-05-29 12:44:35 +0100

    slider dots and autoplay added

--- app/molecule/page.jsx
+++ app/molecule/page.jsx
@@ -8,7 +8,7 @@ import tabs from "../mocks/tabs.json";
 import { CareerCard } from "../_molecules/careerCard";
 import { careers } from "../constants/careers";
 import { SliderImage } from "../_atoms/images";
-import Slider from "../_molecules/slider";
+import { CarouselSlider, ImageSlider } from "../_molecules/slider";
 
 const page = () => {
   return (
@@ -44,19 +44,45 @@ const page = () => {
           ))}
         </div>
       </div>
-      <div>
-        <Slider variant="indefinite">
-          <SliderImage imageLink={"/learner-centered.jpg"}/>
-          <SliderImage />
+      <div className="mt-2">
+        <ImageSlider>
           <SliderImage imageLink={"/1.png"} />
-        </Slider>
+        </ImageSlider>
       </div>
-      <div>
-        <Slider>
+      <div className="mt-2">
+        <ImageSlider variant="infinite" showDots={true}>
+          <SliderImage imageLink={"/learner-centered.jpg"}/>
+          <SliderImage />
+          <SliderImage imageLink={"/1.png"} />
+        </ImageSlider>
+      </div>
+      <div className="mt-2">
+        <ImageSlider showDots={true}>
           <SliderImage imageLink={"/1.png"} />
           <SliderImage imageLink={"/learner-centered.jpg"}/>
           <SliderImage />
-        </Slider>
+        </ImageSlider>
+      </div>
+      <div className="mt-2">
+        <ImageSlider variant={"autoSlide"} showDots={false} showArrows={false}>
+          <SliderImage imageLink={"/1.png"} />
+          <SliderImage imageLink={"/learner-centered.jpg"} />
+          <SliderImage />
+        </ImageSlider>
+      </div>
+      <div className="mt-2">
+        <ImageSlider variant={"autoSlide"} showDots={true} showArrows={false}>
+          <SliderImage imageLink={"/1.png"} />
+          <SliderImage imageLink={"/learner-centered.jpg"} />
+          <SliderImage />
+        </ImageSlider>
+      </div>
+      <div className="mt-2">
+        <ImageSlider variant={"autoSlide"} showDots={false} showArrows={true}>
+          <SliderImage imageLink={"/1.png"} />
+          <SliderImage imageLink={"/learner-centered.jpg"} />
+          <SliderImage />
+        </ImageSlider>
       </div>
     </div>
   );
