76b4fecb2f1cfccd8c4b885a67dabc12d197641b
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2025-06-02 13:58:06 +0100

    image alt can be changed now

--- app/_atoms/images.jsx
+++ app/_atoms/images.jsx
@@ -11,16 +11,16 @@ const getImageSlug = (imageLink) =>
         .replace(/\.[^/.]+$/, "")
     : "generic-image";
 
-export const SliderImage = ({ imageLink }) => {
+export const SliderImage = ({ imageLink, imageAlt }) => {
   const src = useMemo(() => getSrc(imageLink), [imageLink]);
-  const imageSlug = useMemo(() => getImageSlug(imageLink), [imageLink]);
+  const randomImageAlt = useMemo(() => getImageSlug(imageLink), [imageLink]);
   return (
     <div className="mx-auto w-full max-w-[1350px]">
       <div className="custom:block relative h-[250px] w-full overflow-hidden">
         <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[1350px] h-full">
           <Image
             src={src}
-            alt={`slider-image-${imageSlug}`}
+            alt={`slider-image-${imageAlt ? imageAlt : randomImageAlt}`}
             fill
             className={
               src == "/generic-image.png"
@@ -34,15 +34,15 @@ export const SliderImage = ({ imageLink }) => {
   );
 };
 
-export const CardImage = ({ imageLink }) => {
+export const CardImage = ({ imageLink, imageAlt }) => {
   const src = useMemo(() => getSrc(imageLink), [imageLink]);
-  const imageSlug = useMemo(() => getImageSlug(imageLink), [imageLink]);
+    const randomImageAlt = useMemo(() => getImageSlug(imageLink), [imageLink]);
   return (
     <div className="mx-auto w-full">
       <div className="relative w-full aspect-[16/9] overflow-hidden rounded-t-m">
         <Image
           src={src}
-          alt={`card-image-${imageSlug}`}
+          alt={`slider-image-${imageAlt ? imageAlt : randomImageAlt}`}
           fill
           className={
             src == "/generic-image.png"
@@ -55,9 +55,9 @@ export const CardImage = ({ imageLink }) => {
   );
 };
 
-export const LogoImage = ({ imageLink = "", width = 50, height = 20 }) => {
+export const LogoImage = ({ imageLink = "", width = 50, height = 20, imageAlt }) => {
   const src = useMemo(() => getSrc(imageLink), [imageLink]);
-  const imageSlug = useMemo(() => getImageSlug(imageLink), [imageLink]);
+  const randomImageAlt = useMemo(() => getImageSlug(imageLink), [imageLink]);
 
   return (
 
@@ -68,9 +68,8 @@ export const LogoImage = ({ imageLink = "", width = 50, height = 20 }) => {
 
       <Image
         src={src}
-        alt={`logo-image-${imageSlug}`}
+        alt={`slider-image-${imageAlt ? imageAlt : randomImageAlt}`}
         fill
-
         className={`object-contain ${src === "/generic-image.png" ? "bg-gray-300" : ""}`}
 
       />
@@ -78,14 +77,14 @@ export const LogoImage = ({ imageLink = "", width = 50, height = 20 }) => {
   );
 };
 
-export const ProfileImage = ({ imageLink = "" }) => {
+export const ProfileImage = ({ imageLink = "", imageAlt }) => {
   const src = useMemo(() => getSrc(imageLink), [imageLink]);
-  const imageSlug = useMemo(() => getImageSlug(imageLink), [imageLink]);
+   const randomImageAlt = useMemo(() => getImageSlug(imageLink), [imageLink]);
   return (
     <div className="relative w-[80px] h-[80px] overflow-hidden">
       <Image
         src={src}
-        alt={`profile-image-${imageSlug}`}
+        alt={`slider-image-${imageAlt ? imageAlt : randomImageAlt}`}
         fill
         className="object-contain border-2 border-black rounded-full bg-gray-200"
       />
