2e5881752830ab6c70250dc5bb22d33f18f87c85
herrbaha <baha_i@hotmail.com>
2025-06-02 22:54:50 +0200

    fixed conflict6

--- app/molecule/page.jsx
+++ app/molecule/page.jsx
@@ -14,6 +14,8 @@ import { TestimonialCard } from "../_molecules/testimonialCard";
 import { SliderImage } from "../_atoms/images";
 import { CarouselSlider, ImageSlider } from "../_molecules/slider";
 import { TestimonialCard } from "../_molecules/testimonialCard";
+import { SliderImage } from "../_atoms/images";
+import Slider from "../_molecules/slider";
 
 const page = () => {
   return (
