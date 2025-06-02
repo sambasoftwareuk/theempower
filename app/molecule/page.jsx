8c757944ffe8b06bb9d721a6d315b1b8c668adf0
herrbaha <baha_i@hotmail.com>
2025-06-02 23:06:08 +0200

    fixed conflict10

--- app/molecule/page.jsx
+++ app/molecule/page.jsx
@@ -14,8 +14,6 @@ import { TestimonialCard } from "../_molecules/testimonialCard";
 import { SliderImage } from "../_atoms/images";
 import { CarouselSlider, ImageSlider } from "../_molecules/slider";
 import { TestimonialCard } from "../_molecules/testimonialCard";
-import { SliderImage } from "../_atoms/images";
-import { CarouselSlider, ImageSlider } from "../_molecules/slider";
 
 const page = () => {
   return (
