633ee0b831468573aae4701df25944ec15ad4bd9
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2025-06-04 21:54:41 +0100

    spelling misatke resolved

--- app/_molecules/learnCard.jsx
+++ app/_molecules/learnCard.jsx
@@ -7,7 +7,7 @@ export const LearnCard = () => {
       <h2 className="text-2xl font-bold mb-6 text-secondary">
         What you"ll learn
       </h2>
-      <div className="grid gird-cols-1 md:grid-cols-2 gap-x-12 gap-y-4 text-gray-800">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-12 gap-y-4 text-gray-800">
         {leftItems.map((item, index) => (
           <div key={index} className="flex items-start gap-2">
             <span>✓</span>
