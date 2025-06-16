7a2649867da5456a9aa22a5f64b6a5058d0d8c6a
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2025-06-16 13:26:10 +0100

    mobile partly applied

--- app/components/mainTabComponent.jsx
+++ app/components/mainTabComponent.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { OutlinedButton } from '../_atoms/buttons'
 import TabMenu from '../_molecules/tabMenu'
 import { Header1 } from '../_atoms/Headers'
+import { AccordionSection } from '../_molecules/accordionSection'
 
 const MainTabComponent = ({tabs}) => {
   return (
@@ -10,12 +11,19 @@ const MainTabComponent = ({tabs}) => {
                 <Header1>Boggarts lavender robes?</Header1>
                 <p>
                   Hermione Granger Fantastic Beasts and Where to Find Them. Bee in your
-                  bonnet Hand of Glory elder wand, spectacles House Cup Bertie Bott’s
+                  bonnet Hand of Glory elder wand, spectacles House Cup Bertie Botts
                   Every Flavor Beans Impedimenta.
                 </p>
               </div>
-              <div className="my-4">
+              <div className="my-4 hidden md:block">
                 <TabMenu tabs={tabs} />
+              </div>
+              <div className="my-4 block md:hidden">
+                {tabs.map((tab, index) => (
+                  <AccordionSection key={index} title={tab.title} links={tab.content} linkColor='black'/>
+                  
+                ))}
+          
               </div>
               <div className="my-4">
                 <OutlinedButton label="Show All" />
