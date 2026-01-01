5b0676046437e7896e455efeaf288eeb4ce95e5c
herrbaha <baha_i@hotmail.com>
2026-01-01 11:15:07 +0100

    add plus

--- app/_molecules/FooterSection.jsx
+++ app/_molecules/FooterSection.jsx
@@ -1,10 +1,11 @@
 import { SambaLinks } from "../_atoms/SambaLinks";
 import Icon from "../_atoms/Icon";
-import { Globe } from "../_atoms/Icons";
+import { Globe, Plus } from "../_atoms/Icons";
 import footerData from "../mocks/footerData";
 import { LogoImage } from "../_atoms/images";
 import { AccordionSection } from "../_molecules/accordionSection";
 import { Header2, Header3 } from "../_atoms/Headers";
+import { BaseButton } from "../_atoms/buttons";
 
 const FooterSection = ({ bgColor = "bg-gray-900" }) => {
   return (
@@ -29,6 +30,7 @@ const FooterSection = ({ bgColor = "bg-gray-900" }) => {
                 <Header3 className="mb-2 tracking-wide text-[16px] text-white">
                   {section?.title}
                 </Header3>
+
                 <ul className="space-y-0 text-[16px]">
                   {section?.subTitles?.map((text, j) => (
                     <li key={j}>
@@ -38,6 +40,25 @@ const FooterSection = ({ bgColor = "bg-gray-900" }) => {
                     </li>
                   ))}
                 </ul>
+
+                <div className="flex justify-center mt-6 w-1/2">
+                  <BaseButton
+                    className="
+                               w-20 h-20
+                               flex items-center justify-center
+                               bg-gradient-to-br from-primary to-primary500
+                               rounded-full
+                               text-white
+                               shadow-lg
+                               hover:scale-110
+                               hover:shadow-2xl
+                               transition-transform duration-300 ease-in-out
+                             "
+                    aria-label={`Add new item to ${section?.subTitles}`}
+                  >
+                    <Icon variant={Plus} size={36} className="animate-pulse" />
+                  </BaseButton>
+                </div>
               </div>
             ))}
           </div>
