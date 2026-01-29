7b8f7da909b2b7a66c3f2e638b69d3d07af32b80
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2026-01-29 14:20:44 +0000

    db call removed from layout

---
+++ app/_components/FooterDbCall.jsx
@@ -0,0 +1,11 @@
+import { getAllSections } from '@/lib/queries';
+import React from 'react'
+import { Footer } from './footer';
+
+export default async function FooterDbCall() {
+    const sections = await getAllSections("en");
+    
+  return (
+    <Footer sections={sections} />
+  )
+}
